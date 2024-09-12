7c5673963bf98d3090a3f412c52fcb0d1f9efe5b
Adzin Abdul Haris <adzinharis34@gmail.com>
2024-09-12 10:19:28 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\Frontend\HomeController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group([], function () {
+    Route::resource('home', HomeController::class);
+});
+
